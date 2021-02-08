0a2fa0296a22b1a1a25eeac710d74bde8ee5e394
jeewon94 <jayyy1224@gmail.com>
2021-02-08 12:41:56 +0900

    02/08 (월) 일정 관리 어플리케이션 (my schedule) 헤더 작성

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Schedule Dashboard</title>
+
+  <!-- css link -->
+  <link rel="stylesheet" href="/myschedule/css/reset.css">
+  <link rel="stylesheet" href="/myschedule/css/style.css">
+</head>
+<body>
+  <!-- all contents are wrapped inside wrap class -->
+  <div class="wrap">
+    <!-- header -->
+    <header>
+      <div class="center headerWrap">
+        <span></span>
+        <h1>Schedule Dashboard</h1>
+        <div class="mIcon">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+    </header>
+
+    <!-- container -->
+    <div class="container">
+      
+    </div>
+  </div>
+</body>
+</html>
